75e87ac441bc8e8b865827d8ca107ec9d989cbfa
dkschdcks <acc33540226@gmail.com>
2022-09-20 16:10:07 +0900

    commit msg

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /* 길게 설명을 해야 할 때 처음과 끝에 적어주자 */
+//		단축키 ctrl + / 를 입력하면 주석처리 됨
+
+        System.out.println("hello Java World - 1");
+        System.out.println("hello Java World - 2");
+        System.out.println("hello Java World - 3");
+        System.out.println("hello Java World - 4");
+        System.out.println("hello Java World - 5");
+
+        System.out.println("1");
+        System.out.print("2");
+        System.out.printf("3");
+        System.out.println();
     }
 }
